27624b1eb1b14cec1322d3e38ad33f20ecc0c446
Fransisko <fransisko21200saverio@gmail.com>
2024-12-23 10:27:24 +0800

    remove request

--- app/Http/Controllers/Api/PostController.php
+++ app/Http/Controllers/Api/PostController.php
@@ -3,7 +3,6 @@
 namespace App\Http\Controllers\Api;
 
 use App\Models\Post;
-use Illuminate\Http\Request;
 use App\Http\Requests\PostRequest;
 use App\Http\Controllers\Controller;
 use App\Http\Resources\PostResource;
